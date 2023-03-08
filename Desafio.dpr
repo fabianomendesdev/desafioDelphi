7a31f2a932eeec1c82ab021c329babbfa0cddb50
Fabiano Mendes <fabianomendesoficial@gmail.com>
2023-03-08 05:12:35 -0300

    Initial commit

---
+++ Desafio.dpr
@@ -0,0 +1,14 @@
+program Desafio;
+
+uses
+  Vcl.Forms,
+  untFrmPrincipal in 'untFrmPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
